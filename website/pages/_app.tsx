93fbe4263718350eaa2ebbf68fd9ec2ac897aa84
Greg Bergé <berge.greg@gmail.com>
2023-12-22 11:34:06 +0400

    docs(website): setup analytics

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const calSans = localFont({
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       className={`${inter.variable} ${calSans.variable} antialiased font-sans`}
     >
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
